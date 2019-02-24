5c70a29f27430f97f4c98a84809edc9a6607af45
thiago-rezende <bitrateti@gmail.com>
2019-02-23 21:49:12 -0300

    added a basic test code
    added a simple Entity class

--- proj_test/src/main.cpp
+++ proj_test/src/main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include "Entity.h"
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello World!" << std::endl;
+    Engine::Entity *e = new Engine::Entity("Player", "This is the entity of a Player.");
+    std::cout << "Hello from CMake!" << std::endl;
+    e->print_data();
+    delete(e);
+    std::cin.get();
     return 0;
 }
