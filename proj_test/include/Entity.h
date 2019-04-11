66269b6802c9fc953c97be51f2c40e82c5378b0d
Thiago Rezende <bitrateti@gmail.com>
2019-04-11 10:59:57 -0300

    Update Entity.h

--- proj_test/include/Entity.h
+++ proj_test/include/Entity.h
@@ -4,22 +4,22 @@
 
 namespace Engine
 {
-    class Entity
-    {
-    public:
-        Entity(std::string n_name, std::string n_info);
-        ~Entity();
-        inline unsigned int get_id() { return m_id; }
-        inline std::string get_name() { return m_name; }
-        inline std::string get_info() { return m_info; }
-        void print_data();
+class Entity
+{
+  public:
+    Entity(std::string n_name, std::string n_info);
+    ~Entity();
+    unsigned int get_id() { return m_id; }
+    inline std::string get_name() { return m_name; }
+    inline std::string get_info() { return m_info; }
+    void print_data();
 
-    public:
-        static unsigned int s_id_count;
+  public:
+    static unsigned int s_id_count;
 
-    private:
-        unsigned int m_id;
-        std::string m_name;
-        std::string m_info;
-    };
-} // Engine
+  private:
+    unsigned int m_id;
+    std::string m_name;
+    std::string m_info;
+};
+} // namespace Engine
