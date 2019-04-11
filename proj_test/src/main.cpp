98140c04e0521a3b92ddf5e95ba426292031c98b
Thiago Rezende <bitrateti@gmail.com>
2019-04-11 11:00:38 -0300

    Update main.cpp

--- proj_test/src/main.cpp
+++ proj_test/src/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char **argv)
     Engine::Entity *e = new Engine::Entity("Player", "This is the entity of a Player.");
     std::cout << "Hello from CMake!" << std::endl;
     e->print_data();
-    delete(e);
+    delete (e);
     std::cin.get();
     return 0;
 }
