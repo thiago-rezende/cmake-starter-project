bbbee8361c17fa686eed7dd7bb96a17faa4cdd85
Thiago Rezende <bitrateti@gmail.com>
2020-04-25 20:53:20 -0300

    feat(CMake): added config file generation to CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include "pch.h"
+#include "project_config.h"
 
 int main(int argc, char **argv)
 {
     std::cout << "CMake Starter Project!" << std::endl;
+    std::cout << "PRJECT_NAME\t:\t" << PROJECT_NAME << std::endl;
+    std::cout << "PRJECT_VERSION\t:\t" << MyProject_VERSION << std::endl;
     return 0;
 }
---
+++ src/project_config.h
@@ -0,0 +1,8 @@
+#define PROJECT_NAME "MyProject"
+
+#define MyProject_VERSION_MAJOR 1
+#define MyProject_VERSION_MINOR 0
+#define MyProject_VERSION_PATCH 0
+#define MyProject_VERSION_TWEAK 0
+
+#define MyProject_VERSION "1.0.0.0"
