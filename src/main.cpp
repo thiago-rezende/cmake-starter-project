b859353f0afef9d451b7d950828d5b9b28830049
Thiago Rezende <bitrateti@gmail.com>
2020-04-25 20:54:58 -0300

    fix: fixed spelling check in main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char **argv)
 {
     std::cout << "CMake Starter Project!" << std::endl;
-    std::cout << "PRJECT_NAME\t:\t" << PROJECT_NAME << std::endl;
-    std::cout << "PRJECT_VERSION\t:\t" << MyProject_VERSION << std::endl;
+    std::cout << "PROJECT_NAME\t:\t" << PROJECT_NAME << std::endl;
+    std::cout << "PROJECT_VERSION\t:\t" << MyProject_VERSION << std::endl;
     return 0;
 }
