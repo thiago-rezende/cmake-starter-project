e59bea374330db3090d5c95fd4714077ca96bb4f
Thiago Rezende <bitrateti@gmail.com>
2020-04-26 00:02:21 -0300

    feat: added loggin system

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include "pch.h"
 #include "project_config.h"
+#include "log/HorusLog.h"
 
 int main(int argc, char **argv)
 {
-    std::cout << "CMake Starter Project!" << std::endl;
-    std::cout << "PROJECT_NAME\t:\t" << PROJECT_NAME << std::endl;
-    std::cout << "PROJECT_VERSION\t:\t" << MyProject_VERSION << std::endl;
+    // Initialize Horus Logger
+    horus::Log::init();
+
+    H_INFO("CMake Starter Project!");
+    H_INFO("PROJECT_NAME: {}", PROJECT_NAME);
+    H_INFO("PROJECT_VERSION: {}", MyProject_VERSION);
+
+    H_TRACE("--- HORUS LOG ---");
+    H_INFO("That's an info.");
+    H_WARN("That's an warning.");
+    H_ERROR("That's an error.");
+    H_CRITICAL("That's an critical.");
+
     return 0;
 }
