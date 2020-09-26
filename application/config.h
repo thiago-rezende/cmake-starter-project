df2457e8c7ed42d9c25504bdded5d37dc4b25afd
Thiago Rezende <bitrateti@gmail.com>
2020-09-26 18:49:35 -0300

    docs: updated project version string

--- application/config.h
+++ application/config.h
@@ -8,8 +8,8 @@
 
 /* Project Version [SEMVER] */
 #define PROJECT_VERSION_MAJOR 2
-#define PROJECT_VERSION_MINOR 0
+#define PROJECT_VERSION_MINOR 1
 #define PROJECT_VERSION_PATCH 0
 
 /* Porject Version String [SEMVER] */
-#define PROJECT_VERSION "v2.0.0"
+#define PROJECT_VERSION "v2.1.0"
