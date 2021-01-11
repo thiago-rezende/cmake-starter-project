e8426d069aa69c3e3cc504398a849250b0ac1398
Thiago Rezende <bitrateti@gmail.com>
2021-01-11 18:30:53 -0300

    feat: added msvc dynamic runtime support

--- application/pch.h
+++ application/pch.h
@@ -13,3 +13,20 @@
 
 #include <iostream>
 #include <application/core/logger.h>
+
+/* Windows Things */
+#ifdef WIN32
+
+/* Exclude rarely-used services from Windows headers */
+#ifndef VC_EXTRALEAN
+#define VC_EXTRALEAN
+#endif // VC_EXTRALEAN
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif // WIN32_LEAN_AND_MEAN
+
+/* Windows Header */
+#include <Windows.h>
+
+#endif // WIN32
