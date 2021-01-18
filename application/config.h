038878d5586767b48974769cffb8f55cf8282bdc
Thiago Rezende <bitrateti@gmail.com>
2021-01-18 20:19:52 -0300

    docs: update version number

---
+++ application/config.h
@@ -0,0 +1,15 @@
+#pragma once
+
+/* Porject Name */
+#define PROJECT_NAME "CMakeStarterProject"
+
+#define PROJECT_DESCRIPTION "A Simple C++ Starter Project Using CMake"
+#define PROJECT_HOMEPAGE_URL "https://github.com/thiago-rezende/cmake-starter-project"
+
+/* Project Version [SEMVER] */
+#define PROJECT_VERSION_MAJOR 2
+#define PROJECT_VERSION_MINOR 2
+#define PROJECT_VERSION_PATCH 4
+
+/* Porject Version String [SEMVER] */
+#define PROJECT_VERSION "v2.2.4"
