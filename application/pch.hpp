e1918f5b46fa9a685dcf47aa4d744087a22c2802
Thiago Rezende <bitrateti@gmail.com>
2021-02-13 00:12:55 -0300

    refactor: removed unnecessary include file

--- application/pch.hpp
+++ application/pch.hpp
@@ -2,26 +2,11 @@
  * @file pch.hpp
  * @brief Pre-Compiled Header
  *
+ * This file is auto included in all cpp files by cmake
+ *
  */
 
 #pragma once
-
-/* Windows Stuff */
-#ifdef WIN32
-
-/* Exclude rarely-used services from Windows headers */
-#ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN
-#endif // VC_EXTRALEAN
-
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif // WIN32_LEAN_AND_MEAN
-
-/* Windows Header */
-#include <Windows.h>
-
-#endif // WIN32
 
 /* StdLib Stuff */
 #include <mutex>
