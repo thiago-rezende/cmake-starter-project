f3d832ee3adfc56f0f5e0e2867bcd9e52ec6e02f
Thiago Rezende <bitrateti@gmail.com>
2021-02-21 01:52:28 -0300

    docs: update version number

--- application/config.h
+++ application/config.h
@@ -8,8 +8,8 @@
 
 /* Project Version [SEMVER] */
 #define PROJECT_VERSION_MAJOR 3
-#define PROJECT_VERSION_MINOR 0
-#define PROJECT_VERSION_PATCH 5
+#define PROJECT_VERSION_MINOR 1
+#define PROJECT_VERSION_PATCH 0
 
 /* Porject Version String [SEMVER] */
-#define PROJECT_VERSION "v3.0.5"
+#define PROJECT_VERSION "v3.1.0"
