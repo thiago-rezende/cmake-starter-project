6d1db9749be74c3685d79769584a6ad8eb74f910
Thiago Rezende <bitrateti@gmail.com>
2021-05-16 23:54:55 -0300

    feat: improved install routine with no hard-coded prefix and GNUInstallDirs

--- application/config.h
+++ application/config.h
@@ -13,3 +13,38 @@
 
 /* Porject Version String [SEMVER] */
 #define PROJECT_VERSION "v3.1.2"
+
+/* GNU Install Dirs */
+#define CMAKE_INSTALL_BINDIR "bin"                 // user executables (bin)
+#define CMAKE_INSTALL_SBINDIR "sbin"               // system admin executables (sbin)
+#define CMAKE_INSTALL_LIBEXECDIR "libexec"         // program executables (libexec)
+#define CMAKE_INSTALL_SYSCONFDIR "etc"         // read-only single-machine data (etc)
+#define CMAKE_INSTALL_SHAREDSTATEDIR "com" // modifiable architecture-independent data (com)
+#define CMAKE_INSTALL_LOCALSTATEDIR "var"   // modifiable single-machine data (var)
+#define CMAKE_INSTALL_RUNSTATEDIR "var/run"       // New in version 3.9: run-time variable data (LOCALSTATEDIR/run)
+#define CMAKE_INSTALL_LIBDIR "lib"                 // object code libraries (lib or lib64 or lib/<multiarch-tuple> on Debian)
+#define CMAKE_INSTALL_INCLUDEDIR "include"         // C header files (include)
+#define CMAKE_INSTALL_OLDINCLUDEDIR "/usr/include"   // C header files for non-gcc (/usr/include)
+#define CMAKE_INSTALL_DATAROOTDIR "share"       // read-only architecture-independent data root (share)
+#define CMAKE_INSTALL_DATADIR "share"               // read-only architecture-independent data (DATAROOTDIR)
+#define CMAKE_INSTALL_INFODIR "share/info"               // info documentation (DATAROOTDIR/info)
+#define CMAKE_INSTALL_LOCALEDIR "share/locale"           // locale-dependent data (DATAROOTDIR/locale)
+#define CMAKE_INSTALL_MANDIR "share/man"                 // man documentation (DATAROOTDIR/man)`
+#define CMAKE_INSTALL_DOCDIR "share/doc/CMakeStarterProject"       // documentation root (DATAROOTDIR/doc/PROJECT_NAME)
+
+#define CMAKE_INSTALL_FULL_BINDIR "C:/Program Files/CMakeStarterProject/bin"                 // user executables (bin)
+#define CMAKE_INSTALL_FULL_SBINDIR "C:/Program Files/CMakeStarterProject/sbin"               // system admin executables (sbin)
+#define CMAKE_INSTALL_FULL_LIBEXECDIR "C:/Program Files/CMakeStarterProject/libexec"         // program executables (libexec)
+#define CMAKE_INSTALL_FULL_SYSCONFDIR "C:/Program Files/CMakeStarterProject/etc"         // read-only single-machine data (etc)
+#define CMAKE_INSTALL_FULL_SHAREDSTATEDIR "C:/Program Files/CMakeStarterProject/com" // modifiable architecture-independent data (com)
+#define CMAKE_INSTALL_FULL_LOCALSTATEDIR "C:/Program Files/CMakeStarterProject/var"   // modifiable single-machine data (var)
+#define CMAKE_INSTALL_FULL_RUNSTATEDIR "C:/Program Files/CMakeStarterProject/var/run"       // New in version 3.9: run-time variable data (LOCALSTATEDIR/run)
+#define CMAKE_INSTALL_FULL_LIBDIR "C:/Program Files/CMakeStarterProject/lib"                 // object code libraries (lib or lib64 or lib/<multiarch-tuple> on Debian)
+#define CMAKE_INSTALL_FULL_INCLUDEDIR "C:/Program Files/CMakeStarterProject/include"         // C header files (include)
+#define CMAKE_INSTALL_FULL_OLDINCLUDEDIR "/usr/include"   // C header files for non-gcc (/usr/include)
+#define CMAKE_INSTALL_FULL_DATAROOTDIR "C:/Program Files/CMakeStarterProject/share"       // read-only architecture-independent data root (share)
+#define CMAKE_INSTALL_FULL_DATADIR "C:/Program Files/CMakeStarterProject/share"               // read-only architecture-independent data (DATAROOTDIR)
+#define CMAKE_INSTALL_FULL_INFODIR "C:/Program Files/CMakeStarterProject/share/info"               // info documentation (DATAROOTDIR/info)
+#define CMAKE_INSTALL_FULL_LOCALEDIR "C:/Program Files/CMakeStarterProject/share/locale"           // locale-dependent data (DATAROOTDIR/locale)
+#define CMAKE_INSTALL_FULL_MANDIR "C:/Program Files/CMakeStarterProject/share/man"                 // man documentation (DATAROOTDIR/man)
+#define CMAKE_INSTALL_FULL_DOCDIR "C:/Program Files/CMakeStarterProject/share/doc/CMakeStarterProject"                 // documentation root (DATAROOTDIR/doc/PROJECT_NAME)
