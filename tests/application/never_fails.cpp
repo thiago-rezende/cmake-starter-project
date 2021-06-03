ed7b22a7a2633e2089735f9846a6788f525dd5d4
Thiago Rezende <horus.he4rt@gmail.com>
2021-06-03 20:25:43 -0300

    test: removed Catch2, added Doctest and updated the tests

--- tests/application/never_fails.cpp
+++ tests/application/never_fails.cpp
@@ -1,6 +1,11 @@
-#include <catch2/catch_test_macros.hpp>
+#include <doctest/doctest.h>
 
-TEST_CASE("This test should never fail", "[application]")
+int factorial(int number) { return number <= 1 ? number : factorial(number - 1) * number; }
+
+TEST_CASE("Testing the factorial function")
 {
-    REQUIRE(true);
+    CHECK(factorial(1) == 1);
+    CHECK(factorial(2) == 2);
+    CHECK(factorial(3) == 6);
+    CHECK(factorial(10) == 3628800);
 }
